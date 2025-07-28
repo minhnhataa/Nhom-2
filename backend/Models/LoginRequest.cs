98535811c3b41bfa6f90806af42a14b3b9122a62
minhwuan24 <131628689+minhwuan24@users.noreply.github.com>
2025-07-28 16:07:51 +0700

    Update LoginRequest.cs

--- backend/Models/LoginRequest.cs
+++ backend/Models/LoginRequest.cs
@@ -1,8 +1,8 @@
-﻿namespace APIdangkyvadangnhap.Models
+namespace APIdangkyvadangnhap.Models
 {
 	public class LoginRequest
 	{
-		public string Username { get; set; } = "";
-		public string PasswordHash { get; set; } = "";
+		public string Username { get; set; }
+		public string Password { get; set; }  
 	}
 }
